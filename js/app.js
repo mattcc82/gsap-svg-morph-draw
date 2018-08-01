25c3491f107f4af8b53418e322154f7f0cbb2127
Matt Castle <mattcc82@gmail.com>
2018-08-01 14:33:07 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// dom ready
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('dom ready')
+})
+// window resize
+window.addEventListener('resize', function () {
+    console.log('window resized')
+})
